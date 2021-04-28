2f48529d43430114015136623d4ba3de2f978c11
Sevi <correosevi@gmail.com>
2021-04-28 12:04:20 +0100

    Clean Api code for more readable code

--- src/utils/useApi.js
+++ src/utils/useApi.js
@@ -3,10 +3,13 @@ import useDates from './useDates';
 const useApi = () => {
   const [lastYear, currentDate] = useDates();
 
-  //Bare URL
+  //Api configuration: URL,Api key & filters
   const baseUrl = 'https://api.rawg.io/api/';
+  const key = process.env.REACT_APP_API_KEY;
+  const order = '-rating&page_size=10';
+
   //Popular games filter
-  const popularGames = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+  const popularGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=${order}`;
   //Popular games url
   const popularGamesURL = `${baseUrl}${popularGames}`;
 
