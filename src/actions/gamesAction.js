67abc8ddf7b853c4f1b941ed5a0b9d9696b18240
Sevi <correosevi@gmail.com>
2021-04-29 20:31:19 +0100

    Add new filters to games Action

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -2,14 +2,18 @@ import axios from 'axios';
 import useApi from '../utils/useApi';
 
 export const loadGames = () => async (dispatch) => {
-  const [popularGamesURL] = useApi();
+  const [popularGamesURL, upcomingGamesURL, newGamesURL] = useApi();
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL);
+  const upcomingData = await axios.get(upcomingGamesURL);
+  const newGamesData = await axios.get(newGamesURL);
 
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      new: newGamesData.data.results,
     },
   });
 };
