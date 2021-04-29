375937f36932087fa3bf35899276442bbfcec284
Sevi <correosevi@gmail.com>
2021-04-29 20:26:56 +0100

    Add newGames filter to API hook

--- src/utils/useApi.js
+++ src/utils/useApi.js
@@ -8,17 +8,20 @@ const useApi = () => {
   const key = process.env.REACT_APP_API_KEY;
   const orderPopularGames = '-rating';
   const orderUpcomingGames = '-added';
+  const orderNewGAmes = '-release';
   const itemsPerPage = 10;
 
   //Games filter
   const popularGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=${orderPopularGames}&page_size=${itemsPerPage}`;
   const upcomingGames = `games?key=${key}&dates=${currentDate},${nextYear}&ordering=${orderUpcomingGames}&page_size=${itemsPerPage}`;
+  const newGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=${orderNewGAmes}&page_size=${itemsPerPage}`;
 
   //Api filters URL
   const popularGamesURL = `${baseUrl}${popularGames}`;
   const upcomingGamesURL = `${baseUrl}${upcomingGames}`;
+  const newGamesURL = `${baseUrl}${newGames}`;
 
-  return [popularGamesURL, upcomingGamesURL];
+  return [popularGamesURL, upcomingGamesURL, newGamesURL];
 };
 
 export default useApi;
