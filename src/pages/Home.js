3c23c4a0cce08e547d56e876f79899c5dac3b9c1
Sevi <correosevi@gmail.com>
2021-04-30 20:14:03 +0100

    Add styled & motion to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
 import { useEffect } from 'react';
 
 //Redux
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../actions/gamesAction';
+
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+//Components
+import GameCard from '../components/game-card/Index';
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  //Extract data from Store
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
+
   return <div>Home</div>;
 };
 
