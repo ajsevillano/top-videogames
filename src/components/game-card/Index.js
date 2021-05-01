a11474ba67d08ccff70fd57adf59f8af6640b0fc
Sevi <correosevi@gmail.com>
2021-05-01 19:51:33 +0100

    Add shadowBox to GameCard

--- src/components/game-card/Index.js
+++ src/components/game-card/Index.js
@@ -14,9 +14,13 @@ const GameCard = ({ name, release, image }) => {
 
 const StyledGame = styled(motion.div)`
   min-height: 30vh;
-  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
   img {
     width: 100%;
+    height: 40vh;
+    object-fit: cover;
   }
 `;
 
