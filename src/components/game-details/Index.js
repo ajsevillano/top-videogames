a9dec4f811a7d39bf26e01f56e8765e163643271
Sevi <correosevi@gmail.com>
2021-05-04 15:33:33 +0100

    Add game-details maps and html structure

--- src/components/game-details/Index.js
+++ src/components/game-details/Index.js
@@ -5,9 +5,36 @@ import { useSelector } from 'react-redux';
 
 const GameDetails = () => {
   //Extract data from Store
-  const { game, screenShot } = useSelector((state) => state.detail);
+  const { game, screenshot } = useSelector((state) => state.detail);
 
-  return <div></div>;
+  return (
+    <div className="card-shadow">
+      <div className="detail">
+        <div className="stats">
+          <div className="rating">
+            <h3>{game.name}</h3>
+            <p>{game.rating}</p>
+          </div>
+          <div className="info">
+            <h3>Platform</h3>
+            <div className="platforms">
+              {game.platforms.map((data) => (
+                <h3 key={data.platform.id}>{data.platform.name}</h3>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="media">
+          <img src={game.background_image} alt="Image" />
+        </div>
+        <div className="gallery">
+          {screenshot.results.map((screenshot) => (
+            <img src={screenshot.image} key={screenshot.id} alt="game" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GameDetails;
