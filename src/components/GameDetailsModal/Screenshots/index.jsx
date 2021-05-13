b82e62357fe81ff3065517a1e313a099c7d8bbb8
Sevi <correosevi@gmail.com>
2021-05-13 14:54:13 +0100

    Ignore the first screenshot on the screenshots array

--- src/components/GameDetailsModal/Screenshots/index.jsx
+++ src/components/GameDetailsModal/Screenshots/index.jsx
@@ -5,13 +5,15 @@ const Screenshots = ({ screenshot }) => {
   const ResizeScreenshot = (img, size) => useResizeImage(img, size);
   return (
     <div className="gallery">
-      {screenshot?.map((screenshots) => (
-        <img
-          src={ResizeScreenshot(screenshots.image, 1280)}
-          key={screenshots.id}
-          alt="game"
-        />
-      ))}
+      {screenshot?.map((screenshots) =>
+        screenshots.id === -1 ? null : (
+          <img
+            src={ResizeScreenshot(screenshots.image, 1280)}
+            key={screenshots.id}
+            alt="game"
+          />
+        )
+      )}
     </div>
   );
 };
